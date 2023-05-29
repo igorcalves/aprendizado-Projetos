f7d38cea7543547dc4ff8066d38b0bde9eb2dfe2
JBQ Ilha Exames 02 <JBQ.ILHSAD02@rededor.com.br>
2023-05-29 05:45:11 -0300

    Project Complete

--- Projects/src/all/App.java
+++ Projects/src/all/App.java
@@ -1,15 +1,25 @@
 package all;
 
 
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 import all.services.EmployeeService;
 
 public class App {
 
     public static void main(String[] args) {
-        EmployeeService service = new EmployeeService();
+        LocalDate d04 = LocalDate.parse("2023-07-23");
+        LocalDateTime d05 = LocalDateTime.parse("2023-07-23T01:30:26");
+        Instant d06 = Instant.parse("2023-07-23T01:30:26Z");
 
-        service.atomicCreateWithRead();
-        service.allRegistre(); 
+        LocalDate pastWeekLocalDate = d04.minusWeeks(0);
+
+        
+
+        System.out.println("D04 = " + d04);
+        System.out.println("D04 = " + pastWeekLocalDate);
     }
   
 }
