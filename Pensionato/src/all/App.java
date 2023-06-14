46f7300a44d06aa9c73da9a13552bdf4d1292f1b
Igor Alves <igor193@live.com>
2023-06-14 06:23:41 -0300

    initiate enums

--- Pensionato/src/all/App.java
+++ Pensionato/src/all/App.java
@@ -1,20 +1,12 @@
 package all;
 
 
-import java.util.ArrayList;
-import java.util.List;
 
-import all.entities.Employee;
-import all.services.EmployeeService;
-import all.services.pencionatoService;
 
 public class App {
 
     public static void main(String[] args) {
-        EmployeeService service = new EmployeeService();
 
-        service.atomicCreateWithRead();
-        service.allRegistre();    
     }
   
 }
---
+++ Pensionato/src/all/entities/enums/WorkerLevel.java
@@ -0,0 +1,8 @@
+package all.entities.enums;
+
+public enum WorkerLevel {
+    
+    JUNIOR,
+    MID_LEVEL,
+    SENIOR;
+}
