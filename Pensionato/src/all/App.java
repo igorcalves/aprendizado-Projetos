07d285fdbe50050afec433bd2f40e46224fa55e8
Igor Alves <igor193@live.com>
2023-06-16 05:32:44 -0300

    StringBuilder

--- Pensionato/src/all/App.java
+++ Pensionato/src/all/App.java
@@ -1,20 +1,28 @@
 package all;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import all.entities.Employee;
-import all.services.EmployeeService;
-import all.services.pencionatoService;
+import all.entities.Comment;
+import all.entities.Post;
 
 public class App {
 
-    public static void main(String[] args) {
-        EmployeeService service = new EmployeeService();
+    public static void main(String[] args) throws ParseException{
 
-        service.atomicCreateWithRead();
-        service.allRegistre();    
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+
+        Post post = new Post(sdf.parse("21/06/2023 13:53:23"), "Vou Logo Viajar", "Dia 3 do 9 vou viajar com minha familia a negocios", 3);
+
+        post.addComments(new Comment("Boa viagem meu amigo"));
+        post.addComments(new Comment("Cuidado na viagem"));
+        post.addComments(new Comment("Traga presentes"));
+
+
+        System.out.println(post);
+        
+
     }
   
 }
